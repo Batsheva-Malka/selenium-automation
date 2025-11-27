d95eb823173a242be70f2a01332e84fff8b09507
Batsheva-Malka <hand4d30@mbjcomp.org.il>
2025-11-27 14:29:07 +0200

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp() {
+        Assert.assertTrue(true);
+    }
+}
